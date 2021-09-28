0e093c76f38ef05807f9a4684fc04ad156268eac
santiagotroa <santiago.roa@mercadolibre.com>
2021-09-28 15:19:46 -0300

    Added pull to refresh

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native'
 import AppText from '../components/appText'
 import ListItem from '../components/listItem'
 import Constants from 'expo-constants'
 
 export default function MessagesScreen() {
+  const [refreshing, setRefreshing] = useState(false)
 
   const messages = [
     { id: 1, title: 'John Doe', message: 'Is it still available?', image: 'https://picsum.photos/70/70' },
@@ -26,7 +27,9 @@ export default function MessagesScreen() {
           subtitle={item.message}
           onPress={() => console.log("handled message press")}>
         </ListItem>}
-        ItemSeparatorComponent={() => <View style={styles.itemSeparator}></View>}>
+        ItemSeparatorComponent={() => <View style={styles.itemSeparator}></View>}
+        refreshing={refreshing}
+        onRefresh={() => console.log("Backend calls are not yet implemented")}>
       </FlatList>
     </View>
   )
@@ -35,7 +38,8 @@ export default function MessagesScreen() {
 const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
-    marginLeft: 10
+    marginLeft: 10,
+    flex: 1
   },
   titleContainer: {
     alignItems: 'center'
